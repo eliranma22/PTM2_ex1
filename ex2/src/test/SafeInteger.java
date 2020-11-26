9e2488dc19370c7d1afe70b0c77b637d3543611c
Eliran Maman <eliranma2@colman.ac.il>
2020-11-26 20:23:09 +0200

    Add SafeInteger interface for the 2nd ex.

---
+++ ex2/src/test/SafeInteger.java
@@ -0,0 +1,14 @@
+package test;
+
+public interface SafeInteger {
+
+    public SafeInteger increment();
+    public SafeInteger decrement();
+    public SafeInteger add(int add);
+    public SafeInteger pow(int pow);
+    public int getValue();
+
+}
+
+
+
